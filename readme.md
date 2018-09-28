a1d6bfd43200a7e1c0d9e825989f569179420d23
Antop <antop@naver.com>
2018-09-29 00:57:18 +0900

    start webpack + typescript

---
+++ readme.md
@@ -0,0 +1,15 @@
+## TypeScript - Webpack
+
+https://webpack.js.org/guides/installation/
+https://webpack.js.org/guides/getting-started/
+https://webpack.js.org/guides/asset-management/
+https://webpack.js.org/guides/output-management/
+https://webpack.js.org/guides/development/
+https://webpack.js.org/guides/typescript/
+
+## Using Third Party Libraries
+
+http://microsoft.github.io/TypeSearch/
+https://blogs.msdn.microsoft.com/typescript/2016/06/15/the-future-of-declaration-files/
+
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    // https://webpack.js.org/configuration/devtool/
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[hash].[ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            inject: 'body'
+        })
+    ]
+
+};
